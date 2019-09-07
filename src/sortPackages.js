8846247fd7e09837fa5fb79c280969d487b95273
Todd McNeill <toddmcneill@gmail.com>
2019-09-07 04:27:39 -0600

    Implement the recursive function, getOrderedDependencyList
    * Fill in test cases for getOrderedDependencyList
    * Use sinon to spies to ensure the recursion is working. (This means I have to call the function in the code by a reference to its export rather than just using the function name, which is kinda lame.)
    * Replace double quotes with single quotes

--- src/sortPackages.js
+++ src/sortPackages.js
@@ -1,5 +1,5 @@
 /*
-packageList: an array of strings, each containing a colon- and space-separated package name and optionally a dependency, formatted as "packageName: dependency"
+packageList: an array of strings, each containing a colon- and space-separated package name and optionally a dependency, formatted as 'packageName: dependency'
 Returns: a comma-separated list of packages in an order they can be loaded such that no package is loaded until it's dependency is loaded.
 Throws: if a circular dependency is detected.
  */
@@ -28,23 +28,39 @@ function sortPackages (packageList) {
 }
 
 /*
-packageDetails: an object with the structure { packageName, dependency }
-packageDetailsList: an array of objects with the same structure as packageDetails
-dependencyChain: an array a string package names
+packageName: String
+dependency: String
+packageDetailsList: an array of objects with the structure { packageName, dependency }
+usedDependencies: an array a string package names that have already been used in the current chain
 Returns: an array containing all dependencies down the chain needed to include the package found in packageDetails, as well as the package itself
 Throws: if a circular dependency is detected
  */
-function getOrderedDependencyList(packageDetails, packageDetailsList, dependencyChain = []) {
-  // TODO: implement this function.
+function getOrderedDependencyList(packageName, dependency, packageDetailsList, usedDependencies = []) {
+  // If there is no dependency, return an array with just the package name itself.
+  if (!dependency) {
+    return [ packageName ]
+  }
 
-  return []
+  // Check for a circular dependency.
+  const duplicateIndex = usedDependencies.indexOf(dependency)
+  if (duplicateIndex !== -1) {
+    // Display a helpful message to identify the problem.
+    const messageDetails = [ ...usedDependencies, dependency].slice(duplicateIndex).reverse().join(' > ')
+    throw new Error(`Circular dependency found: ${messageDetails}`)
+  }
+
+  // Add the dependency to the list and go deeper.
+  const dependencyDetails = packageDetailsList.find(packageDetails => packageDetails.packageName === dependency)
+  const dependencyChain = module.exports.getOrderedDependencyList(dependencyDetails.packageName, dependencyDetails.dependency, packageDetailsList, [ ...usedDependencies, packageName ])
+
+  // Add the package to the list after its dependencies.
+  return [ ...dependencyChain, packageName ]
 }
 
 function sortPackagesMultipleDependencies (packageList) {
   const packageListDetails = packageList.map(packageEntry => {
-    // This structure allows for multiple dependency support.
-    const [ packageName, dependency ] = packageEntry.split(': ')
-    return { packageName, dependencies: [ dependency ] }
+    const [ packageName, dependencies ] = packageEntry.split(': ')
+    return { packageName, dependencies: dependencies.split(', ') }
   })
 
   // TODO: implement this function later, since multiple dependencies would probably be very useful.
--- test/sortPackages.test.js
+++ test/sortPackages.test.js
@@ -1,62 +1,72 @@
-const { sortPackages, getOrderedDependencyList, sortPackagesMultipleDependencies } = require('../src/sortPackages.js')
+const sortPackagesFunctions = require('../src/sortPackages.js')
+const { sortPackages, getOrderedDependencyList, sortPackagesMultipleDependencies } = sortPackagesFunctions
 const { assert } = require('chai')
+const sinon = require('sinon')
 
 describe('sortPackages', () => {
+  let sandbox
+  before(() => {
+    sandbox = sinon.sandbox.create()
+  })
+  afterEach(() => {
+    sandbox.restore()
+  })
+
   context('sortPackages', () => {
     it('sorts 1 package with no dependency', () => {
-      const input = [ "a: " ]
+      const input = [ 'a: ' ]
       const output = sortPackages(input)
-      const expected = "a"
+      const expected = 'a'
       assert.equal(output, expected)
     })
 
     it('sorts 2 packages with a single dependency already in the correct order', () => {
-      const input = [ "a: ", "b: a" ]
+      const input = [ 'a: ', 'b: a' ]
       const output = sortPackages(input)
-      const expected = "a, b"
+      const expected = 'a, b'
       assert.equal(output, expected)
     })
 
     it('sorts 2 packages with a single dependency not already in the correct order', () => {
-      const input = [ "a: b", "b" ]
+      const input = [ 'a: b', 'b' ]
       const output = sortPackages(input)
-      const expected = "b, a"
+      const expected = 'b, a'
       assert.equal(output, expected)
     })
 
     it('detects a circular dependency of 2 packages', () => {
-      const input = [ "a: b", "b: a" ]
+      const input = [ 'a: b', 'b: a' ]
       assert.throws(() => sortPackages(input))
     })
 
     it('detects a circular dependency of 5 packages', () => {
-      const input = [ "a: b", "b: c", "c: d", "d: e", "e: a" ]
+      const input = [ 'a: b', 'b: c', 'c: d', 'd: e', 'e: a' ]
       assert.throws(() => sortPackages(input))
     })
 
     it('detects a circular dependency that does not include all packages', () => {
-      const input = [ "a: ", "b: c", "c: d", "d: b", "e: a" ]
+      const input = [ 'a: ', 'b: c', 'c: d', 'd: b', 'e: a' ]
       assert.throws(() => sortPackages(input))
     })
 
     it('sorts valid input example 1 from requirements', () => {
       const input = [
-        "KittenService: CamelCaser",
-        "CamelCaser: "
+        'KittenService: CamelCaser',
+        'CamelCaser: '
       ]
       const output = sortPackages(input)
-      const expected = "CamelCaser, KittenService"
+      const expected = 'CamelCaser, KittenService'
       assert.equal(output, expected)
     })
 
     it('sorts valid input example 2 from requirements', () => {
       const input = [
-        "KittenService: ",
-        "Leetmeme: Cyberportal",
-        "Cyberportal: Ice",
-        "CamelCaser: KittenService",
-        "Fraudstream: Leetmeme",
-        "Ice: "
+        'KittenService: ',
+        'Leetmeme: Cyberportal',
+        'Cyberportal: Ice',
+        'CamelCaser: KittenService',
+        'Fraudstream: Leetmeme',
+        'Ice: '
       ]
       const output = sortPackages(input)
       assert(isOutputValid(input, output))
@@ -64,35 +74,69 @@ describe('sortPackages', () => {
 
     it('throws on invalid input example from requirements', () => {
       const input = [
-        "KittenService: ",
-        "Leetmeme: Cyberportal",
-        "Cyberportal: Ice",
-        "CamelCaser: KittenService",
-        "Fraudstream: ",
-        "Ice: Leetmeme"
+        'KittenService: ',
+        'Leetmeme: Cyberportal',
+        'Cyberportal: Ice',
+        'CamelCaser: KittenService',
+        'Fraudstream: ',
+        'Ice: Leetmeme'
       ]
       assert.throws(() => sortPackages(input))
     })
   })
 
   context('getOrderedDependencyList', () => {
-    it('returns the package if there is no dependency')
-    it('throws an error if the package is already in the dependency chain')
-    it('calls itself to follow the dependency chain')
+    it('returns the package if there is no dependency', () => {
+      const output = getOrderedDependencyList('a', null, [])
+      const expected = [ 'a' ]
+      assert.deepEqual(output, expected)
+    })
+    
+    it('throws an error if the package is already in the dependency chain', () => {
+      const troubleFunction = () => getOrderedDependencyList('a', 'b', [], ['b'])
+      assert.throws(troubleFunction, 'Circular dependency found')
+    })
+    
+    it('includes an error message identifying the problem', () => {
+      const troubleFunction = () => getOrderedDependencyList('a', 'c', [], ['b', 'c', 'd', 'e'])
+      assert.throws(troubleFunction, 'Circular dependency found: c > e > d > c')
+    })
+    
+    it('calls itself to follow the dependency chain', () => {
+      sandbox.spy(sortPackagesFunctions, 'getOrderedDependencyList')
+      const packageDetailsList = [
+        { packageName: 'b', dependency: 'c' },
+        { packageName: 'c', dependency: 'd' },
+        { packageName: 'd', dependency: null }
+      ]
+      sortPackagesFunctions.getOrderedDependencyList('a', 'b', packageDetailsList)
+      assert.equal(sortPackagesFunctions.getOrderedDependencyList.callCount, 4)
+    })
+
+    it('returns an ordered dependency chain', () => {
+      const packageDetailsList = [
+        { packageName: 'b', dependency: 'c' },
+        { packageName: 'c', dependency: 'd' },
+        { packageName: 'd', dependency: null }
+      ]
+      const output = getOrderedDependencyList('a', 'b', packageDetailsList)
+      const expected = ['d', 'c', 'b', 'a']
+      assert.deepEqual(output, expected)
+    })
   })
 
   context.skip('sortPackagesMultipleDependencies', () => {
     it('sorts 3 packages with 1 multiple dependency', () => {
-      const input = [ "a: b, c", "b: ", "c: b" ]
+      const input = [ 'a: b, c', 'b: ', 'c: b' ]
       const output = sortPackages(input)
-      const expected = "b, c, a"
+      const expected = 'b, c, a'
       assert.equal(output, expected)
     })
 
     it('sorts 5 packages with each depending on all the ones following it', () => {
-      const input = [ "a: b, c, d, e", "b: c, d, e", "c: d, e", "d: e", "e: " ]
+      const input = [ 'a: b, c, d, e', 'b: c, d, e', 'c: d, e', 'd: e', 'e: ' ]
       const output = sortPackages(input)
-      const expected = "e, d, c, b, a"
+      const expected = 'e, d, c, b, a'
       assert.equal(output, expected)
     })
   })
